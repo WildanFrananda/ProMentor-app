aae01eb45ed85957977a08c9eeeaf95fc332977e
WildanFrananda <wildanrailfanskrl@gmail.com>
2025-12-01 23:00:30 +0700

    feat: Add coach session list and chat bubble UI, and enhance HTTP client with session configuration.

---
+++ ProMentorHQ/Presentation/ViewModels/CoachSessionListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoachSessionListViewModel.swift
+//  ProMentorHQ
+//
+//  Created by Wildan Frananda on 29/11/25.
+//
+
+import Foundation
---
+++ ProMentorHQ/Presentation/Views/Session/CoachSessionListView.swift
@@ -0,0 +1,18 @@
+//
+//  CoachSessionListView.swift
+//  ProMentorHQ
+//
+//  Created by Wildan Frananda on 29/11/25.
+//
+
+import SwiftUI
+
+struct CoachSessionListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoachSessionListView()
+}
